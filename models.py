8343c14edf220437d9243e67b0f8ae3e8bb1d1bc
2M-ai <mayur123mhaske@gmail.com>
2025-01-07 21:42:28 +0530

    1. Added .env and settings 2. Got ouput from mistral 8x7b

---
+++ models.py
@@ -0,0 +1,16 @@
+from langchain_community.llms.huggingface_hub import HuggingFaceHub
+from settings import get_settings
+
+env = get_settings()
+llm = HuggingFaceHub(
+    repo_id = env.MISTRAL8x7b_REPO_ID,
+    huggingfacehub_api_token = env.HF_API_TOKEN,
+    task = "text-generation"
+)
+
+
+prompt_template = "<s> [INST] {input_msg} [/INST] Model answer</s>"
+prompt = "Hi"
+input_prompt = prompt_template.format(input_msg = prompt)
+out = llm.invoke(input_prompt)
+print(out[len(input_prompt):])
---
+++ settings.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+from pydantic_settings import (BaseSettings, SettingsConfigDict)
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding='utf-8'
+    )
+
+    MISTRAL_REPO_ID: str|None = None
+    MISTRAL8x7b_REPO_ID: str|None = None
+    ZEPHYR_REPO_ID: str|None = None
+    HF_API_TOKEN: str|None = None
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
