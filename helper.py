c595e99b8e33a6b795342e373af1285940c4d83e
2M-ai <mayur123mhaske@gmail.com>
2025-01-17 16:21:45 +0530

    1. corrected  Mistral prompt template.

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 def mistral_prompt_template(prompt):
     
-    prompt_template = "<s> [INST] {input_prompt} [/INST] Model answer</s>"
+    prompt_template = "[INST] {input_prompt} [/INST]"
     return prompt_template.format(input_prompt=prompt)
 
 def zephyr_prompt_template(prompt):
